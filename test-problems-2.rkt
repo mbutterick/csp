5a00f58a0060742b8d8772953e061d0e086c381d
Matthew Butterick <mb@mbtype.com>
2015-05-27 11:32:23 -0700

    macroness

--- test-problems-2.rkt
+++ test-problems-2.rkt
@@ -161,20 +161,21 @@
                          (when updated-problem
                            (loop updated-problem)))]))))
 
+(define bts backtracking-solver)
 
 (define/contract (get-solution-generator prob solver)
   (problem? procedure? . -> . generator?)
   (solver prob))
 
 
-(define/contract (get-solution prob solver)
-  (problem? procedure? . -> . (or/c #f assignment?))
-  (define results (get-solutions prob solver #:count 1))
+(define/contract (solve prob [solver backtracking-solver])
+  ((problem?) (procedure?) . ->* . (or/c #f assignment?))
+  (define results (solve* prob solver #:count 1))
   (and (not (empty? results)) (car results)))
 
 
-(define/contract (get-solutions prob solver #:count [count +inf.0])
-  ((problem? procedure?) (#:count natural?) . ->* . (listof assignment?))
+(define/contract (solve* prob [solver backtracking-solver] #:count [count +inf.0])
+  ((problem?) (procedure? #:count natural?) . ->* . (listof assignment?))
   (for/list ([solution (in-producer (get-solution-generator prob solver) (void))]
              ;; #:final is better than #:when because it avoids generating a superfluous solution at the end
              [index (in-naturals)] #:final (= (add1 index) count))
@@ -187,52 +188,42 @@
 
 ;; map problem in AINA
 
-(define map-problem (make-problem
-                   '(WA (r g b)
-                        NT (r g b)
-                        SA (r g b)
-                        Q (r g b)
-                        NSW (r g b)
-                        V (r g b)
-                        T (r g b))
-                   (list
-                    (constraint '(WA NT) (negate equal?))
-                    (constraint '(Q NT) (negate equal?))
-                    (constraint '(SA NT) (negate equal?))
-                    (constraint '(Q NSW) (negate equal?))
-                    (constraint '(NSW V) (negate equal?))
-                    (constraint '(SA V) (negate equal?))
-                    (constraint '(SA Q) (negate equal?))
-                    (constraint '(SA NSW) (negate equal?))
-                    (constraint '(SA WA) (negate equal?)))))
-                         
 
-#;(define-syntax (variables . vdpairs)
-  (#:rest ((cons/c (or/c variable? (listof variable?)) domain?)) . -> . (listof (or/c variable? domain?)))
-  (append*
-   (for/list ([vdpair (in-list vdpairs)])
-     (define var-or-vars (car vdpair))
-     (define domain (cdr vdpair))
-     (if (list? var-or-vars)
-         (append-map (map (λ(var) (list var domain)) var-or-vars))
-         (list var-or-vars domain)))))
-    
 
-#;(define map-problem (make-problem
-                     (variables
-                      ['WA '(r g b)]
-                      ['(NT SA Q NSW V T) '(r g b)]) 
-                     (constraints
-                      ['(WA NT) (negate equal?)]
-                      ['(Q NT) (negate equal?)]
-                      ['(SA NT) (negate equal?)]
-                      ['(Q NSW) (negate equal?)]
-                      ['(NSW V) (negate equal?)]
-                      ['(SA V) (negate equal?)]
-                      ['(SA Q) (negate equal?)]
-                      ['(SA NSW) (negate equal?)]
-                      ['(SA WA) (negate equal?)])))
+(require (for-syntax racket/base racket/syntax racket/list racket/function))
+(define-syntax (make-variables stx)
+  (syntax-case stx ()
+    [(_ vdlists ...)
+     (let ([my-vdlists (syntax->datum #'(vdlists ...))])
+       (define alternating-vars-and-domains
+         (append*
+          (for/list ([vdlist (in-list my-vdlists)])
+            (let* ([var-or-vars (cadar vdlist)]
+                   [domain (cadr vdlist)]
+                   [vars (if (list? var-or-vars) var-or-vars (list var-or-vars))])
+              (append-map (λ(var) (list `(quote ,var) domain)) vars)))))
+       (datum->syntax stx `(list ,@alternating-vars-and-domains)))]))
 
+(define-syntax (make-constraints stx)
+  (syntax-case stx ()
+    [(_ sflists ...) ; sflist = scope-function list
+     (let ([my-sflists (syntax->datum #'(sflists ...))])
+       (define constraints
+         (append*
+          (for/list ([sflist (in-list my-sflists)])
+            (let* ([scope-or-scopes (cadar sflist)]
+                   [function (cadr sflist)]
+                   [scopes (if (andmap list? scope-or-scopes) scope-or-scopes (list scope-or-scopes))])
+              (map (λ(scope) `(constraint (quote ,scope) ,function)) scopes)))))
+       (datum->syntax stx `(list ,@constraints)))]))
+
+
+(define map2
+  (make-problem
+   (make-variables ['(WA NT SA Q NSW V T) '(r g b)])
+   (make-constraints ['((WA NT) (Q NT) (SA NT) (Q NSW) (NSW V) (SA V) (SA Q) (SA NSW) (SA WA)) (negate equal?)])))
+
+(solve map2 backtracking-solver)
 
 ;; ABC problem:
 ;; what is the minimum value of
@@ -244,9 +235,7 @@
 
 
 
-(define abc-problem (make-problem (list "a" (range 1 10)
-                                        "b" (range 10)
-                                        "c" (range 10))))
+(define abc-problem (make-problem (make-variables ['("a" "b" "c") (range 1 10)])))
 (define (test-solution s) (let ([a (solution-ref s "a")]
                                 [b (solution-ref s "b")]
                                 [c (solution-ref s "c")])
@@ -262,10 +251,11 @@
 
 
 
-(define quarter-problem (make-problem
-                         (list "dollars" (range 1 27) "quarters" (range 1 27))
-                         (list (constraint '("dollars" "quarters") (λ(d q) (= 26 (+ d q))))
-                               (constraint '("dollars" "quarters") (λ(d q) (= 17 (+ d (* 0.25 q))))))))
+(define quarter (make-problem
+                 (make-variables ['("dollars" "quarters") (range 1 27)])
+                 (make-constraints
+                  ['("dollars" "quarters") (λ(d q) (= 26 (+ d q)))]
+                  ['("dollars" "quarters") (λ(d q) (= 17 (+ d (* 0.25 q))))])))
 
 ;(get-solution quarter-problem backtracking-solver)
 
@@ -273,23 +263,21 @@
 #|
 A collection of 33 coins, consisting of nickels, dimes, and quarters, has a value of $3.30. If there are three times as many nickels as quarters, and one-half as many dimes as nickels, how many coins of each kind are there?
 |#
-(define nickel-problem (make-problem (list 'nickels (range 1 34)
-                                           'dimes (range 1 34)
-                                           'quarters (range 1 34))
-                                     (list
-                                      (constraint '(nickels) even?)
-                                      (constraint '(nickels dimes quarters) (λ(n d q) (= 33 (+ n d q))))
-                                      (constraint '(nickels dimes quarters) (λ(n d q) (= 3.30 (+ (* 0.05 n) (* 0.1 d) (* 0.25 q)))))
-                                      (constraint '(nickels quarters) (λ(n q) (= n (* 3 q))))
-                                      (constraint '(dimes nickels) (λ(d n) (= n (* 2 d)))))))
+(define nickel (make-problem (make-variables ['(nickels dimes quarters) (range 1 34)])
+                             (make-constraints
+                              ['(nickels) even?]
+                              ['(nickels dimes quarters) (λ(n d q) (= 33 (+ n d q)))]
+                              ['(nickels dimes quarters) (λ(n d q) (= 3.30 (+ (* 0.05 n) (* 0.1 d) (* 0.25 q))))]
+                              ['(nickels quarters) (λ(n q) (= n (* 3 q)))]
+                              ['(dimes nickels) (λ(d n) (= n (* 2 d)))])))
 
 
 (define (quick-test)
   (time-repeat 25
-               (check-hash-items (argmin test-solution (get-solutions abc-problem backtracking-solver))
+               (check-hash-items (argmin test-solution (solve* abc-problem backtracking-solver))
                                  #hash(("c" . 9) ("b" . 9) ("a" . 1)))
-               (check-hash-items (get-solution nickel-problem backtracking-solver) #hash((nickels . 18) (quarters . 6) (dimes . 9)))
-               (check-hash-items (get-solution quarter-problem backtracking-solver) #hash(("dollars" . 14) ("quarters" . 12)))))
+               (check-hash-items (solve nickel backtracking-solver) #hash((nickels . 18) (quarters . 6) (dimes . 9)))
+               (check-hash-items (solve quarter backtracking-solver) #hash(("dollars" . 14) ("quarters" . 12)))))
 
 (define (param-quick-test)
   (parameterize ([current-inference-rule #f]
@@ -346,32 +334,29 @@ A collection of 33 coins, consisting of nickels, dimes, and quarters, has a valu
 #
 |#
 
-#|
+
 (define (make-alldiff-constraints vars)
   (for*/list ([var1 (in-list vars)]
-         [var2 (in-list (cdr (member var1 vars)))])
+              [var2 (in-list (cdr (member var1 vars)))])
     (constraint (list var1 var2) (λ(v1 v2) (not (equal? v1 v2))))))
 
-(define var-names '(l1 l2 l3 l4 r1 r2 r3 r4 x))
-(define var-domains (make-list 9 (range 10)))
+
 (define xsum-problem (make-problem
-  (append-map list var-names var-domains)
-  (list*
-   (constraint '(l1 l2 l3 l4 x) (λ (l1 l2 l3 l4 x) 
-                                       (and (< l1 l2 l3 l4)
-                                            (= 27 (+ l1 l2 l3 l4 x)))))
-   (constraint '(r1 r2 r3 r4 x) (λ (r1 r2 r3 r4 x) 
-                                       (and (< r1 r2 r3 r4)
-                                            (= 27 (+ r1 r2 r3 r4 x)))))
-   (make-alldiff-constraints var-names))))
+                      (make-variables ['(l1 l2 l3 l4 r1 r2 r3 r4 x) (range 10)])
+                      (list*
+                       (constraint '(l1 l2 l3 l4 x) (λ (l1 l2 l3 l4 x) 
+                                                      (and (< l1 l2 l3 l4)
+                                                           (= 27 (+ l1 l2 l3 l4 x)))))
+                       (constraint '(r1 r2 r3 r4 x) (λ (r1 r2 r3 r4 x) 
+                                                      (and (< r1 r2 r3 r4)
+                                                           (= 27 (+ r1 r2 r3 r4 x)))))
+                       (make-alldiff-constraints '(l1 l2 l3 l4 r1 r2 r3 r4 x)))))
 ;(send xsum-problem add-constraint (new all-different-constraint%))
 
-(time (get-solutions xsum-problem backtracking-solver))
+;(time (solve* xsum-problem))
 
 ;(check-equal? (length (send xsum-problem get-solutions)) 8)
 
-
-|#
 
 
 
@@ -394,14 +379,14 @@ A collection of 33 coins, consisting of nickels, dimes, and quarters, has a valu
 (send sm-problem add-constraint (λ(x) (> x 0)) '(m))
 (send sm-problem add-constraint (λ(d e y) (= (modulo (+ d e) 10) y)) '(d e y))
 (send sm-problem add-constraint (λ(n d r e y)
-                                 (= (modulo (+ (word-value n d) (word-value r e)) 100)
-                                    (word-value e y))) '(n d r e y))
+                                  (= (modulo (+ (word-value n d) (word-value r e)) 100)
+                                     (word-value e y))) '(n d r e y))
 (send sm-problem add-constraint (λ(e n d o r y)
-                                 (= (modulo (+ (word-value e n d) (word-value o r e)) 1000) (word-value n e y))) '(e n d o r y))
+                                  (= (modulo (+ (word-value e n d) (word-value o r e)) 1000) (word-value n e y))) '(e n d o r y))
 (send sm-problem add-constraint (λ(s e n d m o r y) (=
-                                                    (+ (word-value s e n d)
-                                                       (word-value m o r e))
-                                                    (word-value m o n e y))) '(s e n d m o r y))
+                                                     (+ (word-value s e n d)
+                                                        (word-value m o r e))
+                                                     (word-value m o n e y))) '(s e n d m o r y))
 (send sm-problem add-constraint (new all-different-constraint%))
 
 (check-hash-items (send sm-problem get-solution) '#hash((m . 1) (e . 5) (r . 8) (n . 6) (y . 2) (o . 0) (d . 7) (s . 9)))
@@ -416,11 +401,11 @@ A collection of 33 coins, consisting of nickels, dimes, and quarters, has a valu
 (send queens-problem add-variables cols rows)
 (for* ([col1 (in-list cols)] [col2 (in-list cols)] #:when (< col1 col2))
   (send queens-problem add-constraint (λ(row1 row2 [col1 col1][col2 col2])
-                           (and 
-                            ;; test if two cells are on a diagonal
-                            (not (= (abs (- row1 row2)) (abs (- col1 col2))))
-                            ;; test if two cells are in same row
-                            (not (= row1 row2)))) (list col1 col2))) 
+                                        (and 
+                                         ;; test if two cells are on a diagonal
+                                         (not (= (abs (- row1 row2)) (abs (- col1 col2))))
+                                         ;; test if two cells are in same row
+                                         (not (= row1 row2)))) (list col1 col2))) 
 (check-equal? (length (send queens-problem get-solutions)) 92)
 
 (module+ main
